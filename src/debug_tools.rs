de2e891f21c3ff729fdf121a1a9881ca1358effe
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2024-11-16 15:20:24 -0500

    Refactor memory mapping

--- src/debug_tools.rs
+++ src/debug_tools.rs
@@ -3,7 +3,7 @@ use std::io::stdin;
 use crate::{cpu::registers::combine, cpu::Cpu};
 
 pub const DEBUG_GRAPHIC: bool = false;
-const TEST_ROM: bool = false;
+const TEST_ROM: bool = true;
 const DEBUG_MODE: bool = false;
 const DEBUG_STOP: bool = false;
 static mut TEST_ROM_MESSAGE: String = String::new();
--- src/memorybus.rs
+++ src/memorybus.rs
@@ -7,40 +7,10 @@ use crate::cpu::timer::Timer;
 use crate::joypad::Joypad;
 use std::{env, fs::File, io::Read};
 
-const ROM_B1_END: u16 = 0x3FFF;
-const ROM_B1_SIZE: u16 = ROM_B1_END + 1;
-
-const ROM_B2_START: u16 = 0x4000;
-const ROM_B2_END: u16 = 0x7FFF;
-const ROM_B2_SIZE: u16 = ROM_B2_END - ROM_B2_START + 1;
-
-const VRAM_START: u16 = 0x8000;
-const VRAM_END: u16 = 0x9FFF;
-const VRAM_SIZE: u16 = VRAM_END - VRAM_START + 1;
-
-const EXTRAM_START: u16 = 0xA000;
-const EXTRAM_END: u16 = 0xBFFF;
-const EXTRAM_SIZE: u16 = EXTRAM_END - EXTRAM_START + 1;
-
-const HRAM_START: u16 = 0xFF80;
-const HRAM_END: u16 = 0xFFFE;
-const HRAM_SIZE: u16 = HRAM_END - HRAM_START + 1;
-
-const WRAM_START: u16 = 0xC000;
-const WRAM_END: u16 = 0xCFFF;
-const WRAM_SIZE: u16 = WRAM_END - WRAM_START + 1;
-
-const W2RAM_START: u16 = 0xD000;
-const W2RAM_END: u16 = 0xDFFF;
-const W2RAM_SIZE: u16 = W2RAM_END - W2RAM_START + 1;
-
-const ECHO_START: u16 = 0xE000;
-const ECHO_END: u16 = 0xFDFF;
-const ECHO_SIZE: u16 = ECHO_END - ECHO_START + 1;
-
-const RESCALE_MIRROR: u16 = WRAM_SIZE + W2RAM_SIZE;
-
-const TOTAL_ROM_SIZE: u16 = ROM_B2_SIZE + ROM_B1_SIZE + 1;
+const TOTAL_ROM_SIZE: u16 = 0x7FFF + 1;
+const VRAM_SIZE: u16 = 0x9FFF - 0x8000 + 1;
+const HRAM_SIZE: u16 = 0xFFFE - 0xFF80 + 1;
+const WRAM_SIZE: u16 = 0xDFFF - 0xC000 + 1;
 const MEM_MAX: u16 = 0xFFFF;
 
 pub struct MemoryBus {
@@ -51,9 +21,6 @@ pub struct MemoryBus {
     vram: [u8; VRAM_SIZE as usize],
     hram: [u8; HRAM_SIZE as usize],
     wram: [u8; WRAM_SIZE as usize],
-    w2ram: [u8; W2RAM_SIZE as usize],
-    echo: [u8; ECHO_SIZE as usize],
-    extram: [u8; EXTRAM_SIZE as usize],
     ie: u8,
     pub pc: u16,
     pub cycle: u128,
@@ -70,9 +37,6 @@ impl MemoryBus {
             vram: [0; VRAM_SIZE as usize],
             hram: [0; HRAM_SIZE as usize],
             wram: [0; WRAM_SIZE as usize],
-            w2ram: [0; W2RAM_SIZE as usize],
-            echo: [0; ECHO_SIZE as usize],
-            extram: [0; EXTRAM_SIZE as usize],
             pc: 0x100,
             ie: 0,
             cycle: 0,
@@ -91,19 +55,10 @@ impl MemoryBus {
             0xFF06 => self.timer.tma,
             0xFF07 => self.timer.tac,
             0xFF0F => self.interrupt.iflag,
-            0..=ROM_B2_END => self.rom[loc as usize],
-            EXTRAM_START..=EXTRAM_END => self.extram[(loc - EXTRAM_START) as usize],
-            VRAM_START..=VRAM_END => self.vram[(loc - VRAM_START) as usize],
-            HRAM_START..=HRAM_END => self.hram[(loc - HRAM_START) as usize],
-            WRAM_START..=WRAM_END => self.wram[(loc - WRAM_START) as usize],
-            W2RAM_START..=W2RAM_END => self.w2ram[(loc - W2RAM_START) as usize],
-            ECHO_START..=ECHO_END => match loc - RESCALE_MIRROR {
-                WRAM_START..=WRAM_END => self.wram[(loc - RESCALE_MIRROR - WRAM_START) as usize],
-                W2RAM_START..=W2RAM_END => {
-                    self.w2ram[(loc - RESCALE_MIRROR - W2RAM_START) as usize]
-                }
-                _ => 0,
-            },
+            0..=0x7FFF => self.rom[loc as usize],
+            0x8000..=0x9FFF => self.vram[(loc - 0x8000) as usize],
+            0xFF80..=0xFFFE => self.hram[(loc - 0xFF80) as usize],
+            0xC000..=0xCFFF => self.wram[(loc - 0xC000) as usize],
             0xFFFF => self.interrupt.ie,
             _ => {
                 println!("Read: memory not handled: {:x}", loc);
@@ -124,21 +79,10 @@ impl MemoryBus {
             0xFF06 => self.timer.tma = value,
             0xFF07 => self.timer.tac = value | 0xF8,
             0xFF0F => self.interrupt.set_iflag(value),
-            0..=ROM_B2_END => self.rom[loc as usize] = value,
-            EXTRAM_START..=EXTRAM_END => self.extram[(loc - EXTRAM_START) as usize] = value,
-            VRAM_START..=VRAM_END => self.vram[(loc - VRAM_START) as usize] = value,
-            HRAM_START..=HRAM_END => self.hram[(loc - HRAM_START) as usize] = value,
-            WRAM_START..=WRAM_END => self.wram[(loc - WRAM_START) as usize] = value,
-            W2RAM_START..=W2RAM_END => self.w2ram[(loc - W2RAM_START) as usize] = value,
-            ECHO_START..=ECHO_END => match loc - RESCALE_MIRROR {
-                WRAM_START..=WRAM_END => {
-                    self.wram[(loc - RESCALE_MIRROR - WRAM_START) as usize] = value
-                }
-                W2RAM_START..=W2RAM_END => {
-                    self.w2ram[(loc - RESCALE_MIRROR - WRAM_START) as usize] = value
-                }
-                _ => {}
-            },
+            0..=0x7FFF => self.rom[loc as usize] = value,
+            0x8000..=0x9FFF => self.vram[(loc - 0x8000) as usize] = value,
+            0xFF80..=0xFFFE => self.hram[(loc - 0xFF80) as usize] = value,
+            0xC000..=0xCFFF => self.wram[(loc - 0xC000) as usize] = value,
             0xFFFF => self.interrupt.set_ie(value),
             _ => println!("Write: memory not handled: {:x}", loc),
         }
